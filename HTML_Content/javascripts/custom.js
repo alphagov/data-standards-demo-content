e7a1d0c41c6b9ff916b0b9c446f61a8b72b550f4
GRH <gareth.heyes@digital.cabinet-office.gov.uk>
2019-06-18 16:29:18 +0100

    Initial Content
    This is my initial content. More detail and self installing content will follow.

---
+++ HTML_Content/javascripts/custom.js
@@ -0,0 +1,121 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+  $('#CSV').hide()
+  $('#XLS').hide()
+})
+
+// GH content
+// This populates the final display field - Generate button
+
+function mydisplay () {
+  $('#creatout').html($('#crein').val())
+  $('#contout').html($('#conin').val())
+  $('#tiout').html($('#tiin').val())
+  $('#createdout').html($('#createdin').val())
+  $('#identout').html($('#ident').val())
+  $('#descout').html($('#desc').val())
+
+  var filedets1 = 'declare-header:' + $('#decheader').val()
+  var filedets2 = 'declare-data:' + $('#decdata').val()
+  var filedets3 = 'declare-data:' + $('#decsheet').val()
+  $('#filedets').html(filedets3 + '\n' + filedets1 + '\n' + filedets2)
+}
+
+// This copies the data
+
+function getMytext () {
+  var texty = (
+    'creator:' + $('#crein').val() + '\n' +
+    'contributor:' + $('#conin').val() + '\n' +
+    'title:' + $('#tiin').val() + '\n' +
+    'created:' + $('#createdin').val() + '\n' +
+    'identifier:' + $('#ident').val() + '\n' +
+    'description:' + $('#desc').val() + '\n' +
+    'declare-datasheet:' + $('#decsheet').val() + '\n' +
+    'declare-header:' + $('#decheader').val() + '\n' +
+    'declare-data:' + $('#decdata').val()
+  )
+return texty
+}
+
+
+function CopyDDF() {
+  // eslint-disable-next-line no-undef
+  texty = getMytext ()
+  var tocopy = $('<textarea>').val(texty).appendTo('body').select()
+  tocopy.select()
+  document.execCommand('copy')
+  alert('Copied to clipboard' + texty)
+}
+
+function saveToFile() {
+  // eslint-disable-next-line no-undef
+  texty = getMytext ()
+
+  alert('Copied to clipboard' + texty)
+}
+
+
+
+// This downloads the data
+
+// Added for the datatable
+
+$(document).ready(function () {
+  $('.add-row').click(function () {
+    var col = $('#col').val()
+    var dtip = $('#dtip :selected').text()
+    var desc = $('#desc').val()
+    var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + col + '</td><td>' + dtip + '</td><td>' + desc + '</td></tr>'
+    $('table tbody').append(markup)
+  })
+
+  // Find and remove selected table rows
+  $('.delete-row').click(function () {
+    $('table tbody').find('input[name="record"]').each(function () {
+      if ($(this).is(':checked')) {
+        $(this).parents('tr').remove()
+      }
+    })
+  })
+})
+
+// Show hide selection
+
+//$('#filetip').on('change', function () {
+//  alert('here');
+//})
+
+$(document).ready(function(){
+  $("#filetip").change(function(){
+   switch (this.value) {
+    case '0':
+      $('#CSV').show()
+      $('#XLS').hide()
+      break
+    case '1':
+      $('#CSV').hide()
+      $('#XLS').show()
+      break
+    case '99':
+      $('#CSV').hide()
+      $('#XLS').hide()
+      break
+  };
+
+  });
+});
+
+// File import text
+
+function getFilePath () {
+  $('input[type=file]').change(function () {
+    var filePath = $('#fileUpload').val()
+  })
+}
---
+++ Python Code/externalfile.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import openpyxl as opxl
+from operator import itemgetter
+
+toclst = []
+colhead = []
+datalst = []
+
+# This gets the text file.
+def gettxt():
+    df = pd.read_csv('/Users/xxx/Projects/ExcelExport/metadata2', header=None)
+    return df
+
+# This will get the file, initial we have a default file, later we will allow them to input
+def getfile():
+    #wb = opxl.load_workbook('/Users/xxx/Downloads/Excel Import Text.xlsx')
+    wb = opxl.load_workbook('/Users/xxx/Projects/ExcelExport/Excel Import Text.xlsx')
+    #wb = opxl.load_workbook('/Users/xxx/Projects/ExcelExport/GTest.xlsx')
+    return wb
+
+def getdataheader(df):
+    myhead = df[df.iloc[:, 0].str.contains('ead')]
+    myhead = myhead.to_string().strip()
+    myhead = myhead[20:]
+    return myhead
+
+def getdata(df):
+    myhead = df[df.iloc[:, 0].str.contains('ata:')]
+    myhead = myhead.to_string().strip()
+    myhead = myhead[18:]
+    return myhead
+
+def returnheader(ws, headerrange):
+    for item in headerrange:
+        item = item.split('.', 1)[-1][:-1]
+        if item.endswith('>'):
+           item = item[:-1]
+        else:
+             item = item
+        if len(item) > 1:
+             colhead.append(ws[item].value)
+
+    return colhead
+
+def returndata(ws, datarange):
+    for item in datarange:
+        item = item.split('.', 1)[-1][:-1]
+
+        if item.endswith('>'):
+            item = item[:-1]
+        elif item.endswith(')'):
+            item = item[:-2]
+        else:
+            item = item
+        if len(item) > 1:
+            datalst.append(ws[item].value)
+    return datalst
+
+def chunks(listy, numy):
+    # For item i in a range that is a length of l,
+    for i in range(0, len(listy), numy):
+        # Create an index range for l of n items:
+        yield listy[i:i + numy]
+
+df = gettxt()
+print (df)
+wb = getfile()
+
+ws = wb['data']
+
+headerrange = getdataheader(df)
+headerrange = str(ws[str(headerrange)]).split(",")
+
+#
+
+colprint = returnheader(ws, headerrange)
+print('The column headers are:')
+print(colprint)
+
+
+#
+#
+datarange = getdata(df)
+datarange = str(ws[str(datarange)]).split(",")
+
+datalist = returndata(ws, datarange)
+
+# #This chunks up the data so that it prints more elegantly
+finalListy = list(chunks(datalist, len(colhead)))
+
+i = 0
+while i < len(finalListy):
+    print(finalListy[i])
+    i += 1
+
+df = pd.DataFrame(finalListy, columns=colprint)
+
+df.to_csv('example.csv', index=False)
+
+
+
+
+
+
